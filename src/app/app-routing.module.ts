dff19078366768e02e2fa4c251028add19fa72ce
a.pe <a.pe@reply.it>
2022-11-21 10:56:32 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DiaryComponent } from './components/diary/diary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", component: DiaryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
