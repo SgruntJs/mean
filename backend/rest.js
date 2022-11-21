2f7dffaf787855a4e9ccbe66fff80f08b0452182
a.pe <a.pe@reply.it>
2022-11-21 18:08:31 +0100

    fix cors headers

--- backend/rest.js
+++ backend/rest.js
@@ -25,7 +25,7 @@ diaryEntries = [
 
   app.use( (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   });
