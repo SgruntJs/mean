6dab359a2986987cd0878f41dea98d996b8eba6a
a.pe <a.pe@reply.it>
2022-11-23 14:18:22 +0100

    add login api and jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const authModelSchema = require('../models/authModel');
+const secretKey = "fjihrwirheiu#@R$dnjf$%dkns@3nfwj4vsv";
 
-router.post('/login', (req, res) => {
-
+router.post('/login', async(req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    await authModelSchema.findOne({username: username}).then( (existUser => {
+        console.log('exist user', existUser);
+        if(existUser && existUser._id) {
+            bcrypt.compare(password, existUser.password, function(err, response){
+                if(!err){
+                    if(response){
+                        const authToken = jwt.sign({_id: existUser._id, username: existUser.username}, secretKey, {
+                            expiresIn: '1h'
+                        })
+                        res.json({status: 'ok', data:{ authToken, response, existUser} })
+                    } else if (!responde){
+                        res.json({status: 'ok', data: {existUser, response}})
+                    }
+                }
+            })
+        }
+    })).catch( err => {
+        res.json({status: 'error', data: 'something went wrong'})
+    })
 });
 
 router.post('/register', async (req, res) => {
