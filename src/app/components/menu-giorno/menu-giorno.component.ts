462bdc932618016b8d6e695b2b8d6048b108987b
a.pe <a.pe@reply.it>
2022-11-24 18:34:15 +0100

    update menu del giorno

--- src/app/components/menu-giorno/menu-giorno.component.ts
+++ src/app/components/menu-giorno/menu-giorno.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, find, from, map, mergeAll, mergeMap, Subject, switchMap, tap } from 'rxjs';
+import { Menu } from 'src/app/models/menu.model';
+import { MenuService } from 'src/app/services/menu/menu.service';
 
 @Component({
   selector: 'app-menu-giorno',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuGiornoComponent implements OnInit {
 
-  constructor() { }
+  oggi!: Date;
+  domani!: Date;
+  menu$ = new Subject<any>;
+  tomorrowMenu : any;
+
+  constructor(private menuSrv: MenuService) { }
 
   ngOnInit(): void {
+    this.oggi = new Date();
+    this.domani = new Date();
+    this.domani.setDate(this.domani.getDate() + 1);
+    this.getNextMenu();
+  }
+
+  getNextMenu() {
+    console.log('this.domani', this.domani.toISOString());
+    const newDomani = this.domani.toISOString().split("T")[0];
+    //25/11/2022
+    //giorno 2022-11-24T00:00:00.000Z
+    this.menuSrv.getOrderedMenu().subscribe(res => {
+      this.menu$.next(res.body);
+    });
+    // this.menuSrv.getOrderedMenu().pipe(
+    //   map(date => date.body.giorno.toISOString().split("T")[0]),
+    //   filter(day => day.giorno === newDomani)
+    // ).subscribe(res => {
+    //   console.log('date concide?', res)
+    // })
+
+    const map$ = this.menu$.pipe(
+      // map(x => { return x[0].giorno.split("T")[0]}),
+      // filter(day => { return day.giorno === newDomani})
+      map( item => {
+        item.forEach((y: any) => {
+        y.giorno = y.giorno.split("T")[0];
+         console.log('y.giorno', y.giorno)
+        });
+        return item
+      }),
+      tap( data => data.filter( (d:any) => d.giorno = newDomani)),
+       mergeMap( x => x),
+      
+       )
+      
+    map$.subscribe((data:any) => {
+      this.tomorrowMenu = data;
+    
+    });
   }
 
 }
+
+
