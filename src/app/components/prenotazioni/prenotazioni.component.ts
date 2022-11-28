92d0d3f0f9cf2d52ca631dd5fa5f0c0d1b6a80ff
a.pe <a.pe@reply.it>
2022-11-28 19:22:54 +0100

    fix filter table

--- src/app/components/prenotazioni/prenotazioni.component.ts
+++ src/app/components/prenotazioni/prenotazioni.component.ts
@@ -2,10 +2,9 @@ import { Component, OnInit, PipeTransform } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { FormControl } from '@angular/forms';
 
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { map, mergeMap, startWith } from 'rxjs/operators';
 import { Pasto } from 'src/app/models/orderPasto';
-import { ApicallService } from 'src/app/services/apicall/apicall.service';
 import { MenuService } from 'src/app/services/menu/menu.service';
 
 
@@ -20,24 +19,49 @@ export class PrenotazioniComponent implements OnInit {
   prenotazioni!: Pasto[];
   prenotazioni$!: Observable<Pasto[]>;
 	filter = new FormControl('', { nonNullable: true });
+  ordini$= new Subject<Pasto[]>;
 
-	constructor(pipe: DecimalPipe, private menuSrv: MenuService ) {
-    this.menuSrv.receiveOrder().subscribe( res => {
-      console.log('res', res.pasti);
-      this.prenotazioni = res.pasti;
-    });
-    this.prenotazioni$ = this.menuSrv.receiveOrder().pipe(
-      map( data => data.pasti)
+	constructor(private menuSrv: MenuService ) {
+    this.prenotazioni$ =  this.filter.valueChanges.pipe(
+      startWith(''),
+      map((text) => this.search(text)),
     );
-    console.log('this.prenotazioni$', this.prenotazioni$);
+     this.prenotazioni$ = this.menuSrv.receiveOrder().pipe(
+        map( data => data.pasti)
+      ); 
 	}
 
   ngOnInit(): void {
-    
+    this.menuSrv.receiveOrder().subscribe( res => { 
+      this.prenotazioni = res.pasti;
+      this.ordini$.next(this.prenotazioni);
+    });
+
   }
 
 
+  search(text: string): any {
 
+      // return this.ordini.filter((res) => {
+      //  //  console.log('filter res', res);
+      //   const term = text.toLowerCase();
+      //   return (
+      //     new Date(res.dataPasto).toISOString().includes(term)
+      //   );
+      // });
+      this.ordini$.subscribe(
+         data => {
+          data.filter( res => {
+            const term = text.toLowerCase();
+            return (
+                   new Date(res.dataPasto).toISOString().includes(term)
+                 );
+          } )
+         }
+      )
+
+   
+  }
 
 
 
